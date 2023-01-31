7c67da9d9f1f0d2346d5c7cfdb831ca6bb50aa82
Limsq <limchumzi99@gmail.com>
2023-01-31 21:43:47 +0900

    jsp test commit

---
+++ source/src/main/java/kh/project/stayfit/organic/controller/OganicTemplateController.java
@@ -0,0 +1,46 @@
+package kh.project.stayfit.organic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class OganicTemplateController {
+	@GetMapping("all")
+	public String all() {
+		return "oganic/all";
+	}
+	@GetMapping("blog-details")
+	public String blog_details() {
+		return "oganic/blog-details";
+	}
+	@GetMapping("blog")
+	public String blog() {
+		return "oganic/blog";
+	}
+	@GetMapping("checkout")
+	public String checkout() {
+		return "oganic/checkout";
+	}
+	@GetMapping("contact")
+	public String contact() {
+		return "oganic/contact";
+	}
+	@GetMapping("index")
+	public String index() {
+		return "oganic/index";
+	}
+	@GetMapping("shop-details")
+	public String shop_details() {
+		return "oganic/shop-details";
+	}
+	@GetMapping("shop-grid")
+	public String shop_grid() {
+		return "oganic/shop-grid";
+	}
+	@GetMapping("shop-cart")
+	public String shop_cart() {
+		return "oganic/shop-cart";
+	}
+}
---
+++ source/src/main/webapp/WEB-INF/oganic/all.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="index">index</a>
+<hr>
+<a href="blog_details">blog_details</a>
+<hr>
+<a href="blog">blog</a>
+<hr>
+<a href="checkout">checkout</a>
+<hr>
+<a href="contact">contact</a>
+<hr>
+<a href="shop_details">shop_details</a>
+<hr>
+<a href="shop_grid">shop_grid</a>
+<hr>
+<a href="shoping_cart">shoping_cart</a>	
+</body>
+</html>
