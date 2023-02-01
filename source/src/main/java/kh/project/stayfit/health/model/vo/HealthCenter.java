4a08c8761de536ad67d2b41f26ecb8d530943e31
MinJiPark <pminji1@gmail.com>
2023-02-02 02:39:25 +0900

    HealthCenter vo 작성

--- source/src/main/java/kh/project/stayfit/health/model/vo/HealthCenter.java
+++ source/src/main/java/kh/project/stayfit/health/model/vo/HealthCenter.java
@@ -8,5 +8,14 @@ import lombok.ToString;
 @Getter
 @Setter
 public class HealthCenter {
+//	"HPNUM"	VARCHAR2(15)		NOT NULL,
+//	"HID"	NUMBER		NOT NULL,
+//	"HPLACE"	VARCHAR2(50)		NOT NULL,
+//	"HADDR"	VARCHAR2(100)		NOT NULL
+
+	private String hpnum;
+	private int hid;
+	private String hplace;
+	private String haddr;
 	
 }
