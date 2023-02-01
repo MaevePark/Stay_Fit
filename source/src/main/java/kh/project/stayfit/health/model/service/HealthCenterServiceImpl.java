30ba2c8279e94e7ab6934d09af8d8174e7c4a204
MinJiPark <pminji1@gmail.com>
2023-02-02 03:27:51 +0900

    healthCenterImpl Service 수정

--- source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterServiceImpl.java
@@ -1,8 +1,13 @@
 package kh.project.stayfit.health.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class HealthCenterServiceImpl {
 
+@Service
+public class HealthCenterServiceImpl implements HealthCenterService {
+	@Autowired
+	private HealthCenterService dao;
+	
+	//override -mjpark
 }
