ba90fcc577f459242d85c7016014cbea8c399c12
MinJiPark <pminji1@gmail.com>
2023-02-01 20:07:01 +0900

    @RequestMapping 삭제

--- source/src/main/java/kh/project/stayfit/health/model/dao/HealthCenterDao.java
+++ source/src/main/java/kh/project/stayfit/health/model/dao/HealthCenterDao.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+
 public class HealthCenterDao {
 	@Autowired
 	private SqlSession sqlSession;
