7d37ebb739e8339b8a36e52ced39014c109ab69b
MinJiPark <pminji1@gmail.com>
2023-02-02 20:45:28 +0900

    GraphService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/diary/model/service/GraphService.java
+++ source/src/main/java/kh/project/stayfit/diary/model/service/GraphService.java
@@ -1,5 +1,10 @@
 package kh.project.stayfit.diary.model.service;
 
-public interface GraphService {
+import java.util.List;
 
+import kh.project.stayfit.diary.model.vo.Diary;
+
+public interface GraphService {
+	//그래프 출력
+	public List<Diary> selectList(int a , String b) throws Exception; // select(int, String): List<Diary>
 }
--- source/src/main/java/kh/project/stayfit/diary/model/service/GraphServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/diary/model/service/GraphServiceImpl.java
@@ -1,14 +1,24 @@
 package kh.project.stayfit.diary.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kh.project.stayfit.diary.model.dao.GraphDao;
+import kh.project.stayfit.diary.model.vo.Diary;
 
 @Service
 public class GraphServiceImpl implements GraphService {
 	@Autowired
 	private GraphDao dao;
 	
-	//override -mjpark
+	//그래프 출력
+	@Override
+	public List<Diary> selectList(int a, String b) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
