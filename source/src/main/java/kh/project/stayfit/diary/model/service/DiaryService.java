bbd87df50c495b6439e87be99afdd3ce9e6aba28
MinJiPark <pminji1@gmail.com>
2023-02-02 20:45:02 +0900

    DiaryService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/diary/model/service/DiaryService.java
+++ source/src/main/java/kh/project/stayfit/diary/model/service/DiaryService.java
@@ -1,5 +1,17 @@
 package kh.project.stayfit.diary.model.service;
 
-public interface DiaryService {
+import java.util.List;
 
+import kh.project.stayfit.diary.model.vo.Diary;
+
+public interface DiaryService {
+	//캘린더 작성
+	public int insert() throws Exception; // insert(??)
+	//캘린더 출력
+	public List<Diary> selectList(String a, int b) throws Exception; // select(String, int): List<Diary>
+	//캘린더 수정
+	public int update(String a, int b, int c) throws Exception; // update(String, int, 수정한 파라미터): int
+	//캘린더 삭제
+	public int delete(String a, int b) throws Exception; // delete(String, int): int
+	
 }
