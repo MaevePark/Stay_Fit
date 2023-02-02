4b3382bb120aff97b1973dad049ee53aae486aa6
MinJiPark <pminji1@gmail.com>
2023-02-03 00:59:57 +0900

    마이페이지 MyProductService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/service/MyProductService.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/service/MyProductService.java
@@ -1,5 +1,22 @@
 package kh.project.stayfit.mypage.model.service;
 
-public interface MyProductService {
+import java.util.List;
 
+public interface MyProductService {
+	
+	//TODO List <Wish>, <Basket>, <Bought> 담당자 확인 후 수정
+	//TODO selectTotalCnt 담당자 확인 후 수정
+	
+	//찜목록 호출
+//	public List<Wish> selectProductList(int a, int b, String c) throws Exception; //selectProductList(int, int, String): List<Wish>
+//	public int selectTotalCnt(int a, int b, String c) throws Exception; //selectTotalCnt(int, int, String): int
+	
+	//장바구니 호출
+//	public List<Basket> selectProductList(int a, int b, String c) throws Exception; //selectProductList(int, int, String): List<Basket>
+//	public int selectTotalCnt(int a, int b, String c) throws Exception; //selectTotalCnt(int, int, String): int
+	
+	//구매기록 호출
+//	public List<Bought> selectProductList(int a, int b, String c) throws Exception; //selectProductList(int, int, String): List<Bought>
+//	public int selectTotalCnt(int a, int b, String c) throws Exception; //selectTotalCnt(int, int, String): int
+	
 }
--- source/src/main/java/kh/project/stayfit/mypage/model/service/MyProductServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/service/MyProductServiceImpl.java
@@ -1,8 +1,16 @@
 package kh.project.stayfit.mypage.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kh.project.stayfit.mypage.model.dao.MyProductDao;
+
 @Service
-public class MyProductServiceImpl {
+public class MyProductServiceImpl implements MyProductService {
+	@Autowired
+	private MyProductDao dao;
+	
+	//TODO 담당자가 MyProductService 해결 후 override
+	
 
 }
