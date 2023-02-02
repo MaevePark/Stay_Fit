fc562033e5d7fd2ec2514c82b00af56ccc305c69
MinJiPark <pminji1@gmail.com>
2023-02-03 00:57:39 +0900

    마이페이지 MyProductDao 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/dao/MyProductDao.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/dao/MyProductDao.java
@@ -1,5 +1,36 @@
 package kh.project.stayfit.mypage.model.dao;
 
-public class MyProductDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class MyProductDao {
+	@Autowired
+	private SqlSession sqlsession;
+	
+	//찜목록 호출
+//	public List<Wish> selectProductList(int a, int b, String c) throws Exception {
+//		return null;
+//	}
+//	public int selectTotalCnt(int a, int b, String c) throws Exception {
+//		return 0;
+//	}
+	
+	//장바구니 호출
+//	public List<Basket> selectProductList(int a, int b, String c) throws Exception {
+//		return null;
+//	}
+//	public int selectTotalCnt(int a, int b, String c) throws Exception {
+//		return 0;
+//	}
+	
+	//구매기록 호출
+//	public List<Bought> selectProductList(int a, int b, String c) throws Exception {
+//		return null;
+//	} 
+//	public int selectTotalCnt(int a, int b, String c) throws Exception {
+//		return 0;
+//	} 
+	
 }
