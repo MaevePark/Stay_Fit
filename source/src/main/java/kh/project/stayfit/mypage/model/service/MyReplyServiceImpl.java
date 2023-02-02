cf7164c0c857f5d2f62b98ec06c9c284a36959a0
MinJiPark <pminji1@gmail.com>
2023-02-03 01:00:39 +0900

    마이페이지 MyReplyService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/service/MyReplyServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/service/MyReplyServiceImpl.java
@@ -1,8 +1,14 @@
 package kh.project.stayfit.mypage.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MyReplyServiceImpl {
+import kh.project.stayfit.mypage.model.dao.MyReplyDao;
 
+@Service
+public class MyReplyServiceImpl implements MyReplyService {
+	@Autowired
+	private MyReplyDao dao;
+	
+	//TODO 담당자가 MyReplyService 해결 후 override
 }
