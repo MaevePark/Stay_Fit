74e39eb07fb42fb8140c28a98043f323203b780b
MinJiPark <pminji1@gmail.com>
2023-02-03 00:59:19 +0900

    마이페이지 ProfileService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/service/ProfileServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/service/ProfileServiceImpl.java
@@ -1,8 +1,26 @@
 package kh.project.stayfit.mypage.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import kh.project.stayfit.mypage.model.dao.ProfileDao;
 
 @Service
-public class ProfileServiceImpl {
-
+public class ProfileServiceImpl implements ProfileService {
+	@Autowired
+	private ProfileDao dao;
+	
+	//사용자 정보 변경
+	@Override
+	public int editProfile(String a, String b, MultipartFile c, int d) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//계정 탈퇴
+	@Override
+	public int delProfile(int a) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
