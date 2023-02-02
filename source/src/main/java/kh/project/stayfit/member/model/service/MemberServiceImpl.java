f721db2ad95269190b2a555b7c526c4dfd894a6e
MinJiPark <pminji1@gmail.com>
2023-02-02 18:13:09 +0900

    MemberService 인터페이스 구현

--- source/src/main/java/kh/project/stayfit/member/model/service/MemberServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/member/model/service/MemberServiceImpl.java
@@ -1,8 +1,31 @@
 package kh.project.stayfit.member.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberServiceImpl {
+import kh.project.stayfit.member.model.dao.MemberDao;
 
+@Service
+public class MemberServiceImpl implements MemberService {
+	@Autowired
+	private MemberDao dao;
+	
+	@Override
+	public int login(String a, String b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insert(String a, String b, String c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mailChk(String a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
