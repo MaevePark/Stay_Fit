06a00dc70a2dfe9f275e522c2a0cea48efca20b0
HJ <down012459@gmail.com>
2023-02-02 20:18:30 +0900

    Shop dao, service 초안

--- source/src/main/java/kh/project/stayfit/shop/model/service/ShopServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/shop/model/service/ShopServiceImpl.java
@@ -9,5 +9,46 @@ import kh.project.stayfit.shop.model.dao.ShopDao;
 public class ShopServiceImpl implements ShopService{
 	@Autowired
 	private ShopDao dao;
+	
+//	//쇼핑몰 페이지
+//	//카테고리, 페이징
+//	@Override
+//	public List<Product> selectProduct(String, String, int, int, int, int) throws Exception{
+//		return ;
+//	}
+//	@Override
+//	public int selectTotalCnt(String, String, int, int) throws Exception{
+//		return ;
+//	}
+//	//추천 상품
+//	@Override
+//	public List<Product> selectCustomProduct(int, ???) throws Exception{
+//		return ;
+//	}
+
+	
+//	//상품 조건별 출력(검색), 페이징
+//	@Override
+//	public List<Product> selectProduct(String, String, int, int, int, int) throws Exception{
+//		return ;
+//	}
+//	@Override
+//	public int selectTotalCnt(String, String, int, int) throws Exception{
+//		return ;
+//	}
+
+	
+//	//찜 목록에 추가
+//	@Override
+//	public int addWish(int, int, int) throws Exception{
+//		return ;
+//	}
+
+		
+//	//장바구니에 추가
+//	@Override
+//	public int addBasket(int, int, int) throws Exception{
+//		return ;
+//	}
 
 }
