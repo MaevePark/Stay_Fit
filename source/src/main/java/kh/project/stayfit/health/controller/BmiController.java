0759e05f91d62186a2a391ec27faad667d513a8a
ChocoJ1721031 <qudwn980226@naver.com>
2023-02-02 19:04:08 +0900

    Revert "BM"
    This reverts commit 77c546afb189e02b7eeb004ecfb1dcf71e47c726.

--- source/src/main/java/kh/project/stayfit/health/controller/BmiController.java
+++ source/src/main/java/kh/project/stayfit/health/controller/BmiController.java
@@ -1,28 +1,5 @@
 package kh.project.stayfit.health.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-@RequestMapping("/health")
 public class BmiController {
-	
-	@GetMapping("/bmimeasure")
-	public ModelAndView bmiMeasure(ModelAndView mv) {
-		
-		mv.addObject("sectionName", "health/bmimeasure.jsp");
-		mv.setViewName("index");
-		
-		return mv;
-	}
-	@GetMapping("/bmiresult")
-	public ModelAndView bmiResult(ModelAndView mv) {
-		
-		mv.addObject("sectionName", "health/bmiresult.jsp");
-		mv.setViewName("index");
-		
-		return mv;
-	}
+
 }
