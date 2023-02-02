bff280fdee46cb5c8902090791a58e73db2b6975
MinJiPark <pminji1@gmail.com>
2023-02-03 01:00:16 +0900

    마이페이지 MyBoardService 인터페이스 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/service/MyBoardService.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/service/MyBoardService.java
@@ -1,5 +1,19 @@
 package kh.project.stayfit.mypage.model.service;
 
-public interface MyBoardService {
+import java.util.List;
 
+import kh.project.stayfit.board.model.vo.Bmark;
+import kh.project.stayfit.board.model.vo.Board;
+
+public interface MyBoardService {	
+
+	//TODO selectTotalCnt 담당자 확인 후 수정
+	
+	//작성글 호출
+//	public List<Board> selectBoardList(int a, int b, String c) throws Exception; //selectBoardList(int, int, String): List<Board>
+//	public int selectTotalCnt(int a, int b, String c) throws Exception; //selectTotalCnt(int, int, String): int
+	
+	//북마크 호출
+//	public List<Bmark> selectBoardList(int a, int b, String c) throws Exception; //selectBoardList(int, int, String): List<Bmark>
+//	public int selectTotalCnt(int a, int b, String c) throws Exception; //selectTotalCnt(int, int, String): int
 }
