b90ed667a86e6c26fbe1d5e2e18f91075fcd4ac9
HJ <down012459@gmail.com>
2023-02-02 19:28:13 +0900

    Board Dao 초안

--- source/src/main/java/kh/project/stayfit/board/model/dao/BoardDao.java
+++ source/src/main/java/kh/project/stayfit/board/model/dao/BoardDao.java
@@ -9,4 +9,53 @@ public class BoardDao {
 	@Autowired
 	private SqlSession sqlsession;
 
+//	//커뮤니티 글목록 페이지,페이징
+//	public List<Board> selectList(int, int) throws Exception{
+//		return sqlsession.;
+//	}
+//	public int selectTotalCnt() throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시판 글 등록
+//	public int write(Board) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시판 글 검색, 페이징
+//	public List<Board> selectList(int ,int , String ) throws Exception{
+//		return sqlsession.;
+//	}
+//	public int selectTotalCnt(String) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시글 수정
+//	public int update(Board ) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시글 삭제
+//	public int delete(int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시글 상세페이지
+//	public List<Board> read(int ) throws Exception{
+//		return sqlsession.;
+//	}
+//	public List<Reply> seletList(int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시글 북마크
+//	public int bmark(int ,String , ???) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//게시글 반응
+//	public int emotion(int ,int ,int ) throws Exception{
+//		return sqlsession.;
+//	}	
+
 }
--- source/src/main/java/kh/project/stayfit/board/model/dao/ReplyDao.java
+++ source/src/main/java/kh/project/stayfit/board/model/dao/ReplyDao.java
@@ -9,4 +9,39 @@ public class ReplyDao {
 	@Autowired
 	private SqlSession sqlsession;
 
+//	//댓글 작성
+//	public int write(Reply) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//대댓글 작성
+//	public int answer(Reply) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//댓글 수정
+//	public int update(Reply, int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//댓글 삭제
+//	public int delete(int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//댓글 신고
+//	public int report(Reply, int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//댓글 좋아요
+//	public int like(int, int) throws Exception{
+//		return sqlsession.;
+//	}
+//	
+//	//답변 선택
+//	public int adopt(int, int, int, int) throws Exception{
+//		return sqlsession.;
+//	}
+	
 }
