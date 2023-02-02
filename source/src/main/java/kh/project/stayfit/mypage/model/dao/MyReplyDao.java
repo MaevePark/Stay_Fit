0ce5c469342d98b9d362c31cfa21d0997d4731bf
MinJiPark <pminji1@gmail.com>
2023-02-03 00:58:18 +0900

    마이페이지 MyReplyDao 초안

--- source/src/main/java/kh/project/stayfit/mypage/model/dao/MyReplyDao.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/dao/MyReplyDao.java
@@ -1,5 +1,26 @@
 package kh.project.stayfit.mypage.model.dao;
 
-public class MyReplyDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class MyReplyDao {
+	@Autowired
+	private SqlSession sqlsession;
+	
+	//공감 댓글 호출
+//	public List<LikeReply> selectLikeReplyList(댓글ID, 회원ID, 검색어) throws Exception {
+//		return null;
+//	} //selectLikeReplyList(댓글ID, 회원ID, 검색어): List<LikeReply> - 담당자가 수정
+//	public int selectTotalCnt(int a, int b, String c) throws Exception {
+//		return 0;
+//	}
+	//작성한 댓글 호출
+//	public List<Reply> selectReplyList(int a, String b) throws Exception {
+//		return null;
+//	}
+//	public int selectTotalCnt(int a, String b) throws Exception {
+//		return 0;
+//	}
 }
