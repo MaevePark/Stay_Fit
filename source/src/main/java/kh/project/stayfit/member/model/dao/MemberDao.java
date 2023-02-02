d3df4019e45c4fed440aa1b582247c617dda34a0
MinJiPark <pminji1@gmail.com>
2023-02-02 18:16:03 +0900

    MemberDao 초안

--- source/src/main/java/kh/project/stayfit/member/model/dao/MemberDao.java
+++ source/src/main/java/kh/project/stayfit/member/model/dao/MemberDao.java
@@ -1,5 +1,22 @@
 package kh.project.stayfit.member.model.dao;
 
-public class MemberDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class MemberDao {
+	@Autowired
+	private SqlSession sqlsession;
+	
+	public int login(String a, String b) { // login(String, String): int
+		return 0; 
+	}
+	public int insert(String a ,String b , String c) { // insert(String, String, String): int
+		return 0; 
+	}
+	public int mailChk(String a) { // mailChk(String): int
+		return 0;
+	} 
+	
 }
