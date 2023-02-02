daf2413e14564932c828740b49daa4c07516f24e
MinJiPark <pminji1@gmail.com>
2023-02-02 17:16:31 +0900

    HealthCenterService 인터페이스 작성

--- source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterServiceImpl.java
+++ source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterServiceImpl.java
@@ -1,13 +1,23 @@
 package kh.project.stayfit.health.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kh.project.stayfit.health.model.vo.HealthCenter;
 
 
 @Service
 public class HealthCenterServiceImpl implements HealthCenterService {
 	@Autowired
 	private HealthCenterService dao;
+
+	@Override
+	public List<HealthCenter> selectList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	//override -mjpark
+	
 }
