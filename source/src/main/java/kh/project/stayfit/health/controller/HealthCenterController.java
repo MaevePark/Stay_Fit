dc4aff56728f0e0004f7c40a5f46d1230daa7e16
MinJiPark <pminji1@gmail.com>
2023-02-06 22:35:56 +0900

    HealthCenterService 에 대한 주석 추가

--- source/src/main/java/kh/project/stayfit/health/controller/HealthCenterController.java
+++ source/src/main/java/kh/project/stayfit/health/controller/HealthCenterController.java
@@ -1,13 +1,19 @@
 package kh.project.stayfit.health.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import kh.project.stayfit.health.model.service.HealthCenterService;
+
 @Controller
 @RequestMapping("/health")
 public class HealthCenterController {
+	
+//	@Autowired
+//	private HealthCenterService service;
 	
 	@GetMapping("/centerlist")
 	public ModelAndView healthCenter(ModelAndView mv) {
