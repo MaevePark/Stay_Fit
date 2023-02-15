e2b375bc9317698c161b1f8a0a870dd0840ec271
ChocoJ1721031 <qudwn980226@naver.com>
2023-02-15 20:58:40 +0900

    fatsecret 사이트 영양성분 크롤링 코드 1차

--- source/src/main/java/kh/project/stayfit/common/Crawling.java
+++ source/src/main/java/kh/project/stayfit/common/Crawling.java
@@ -1,8 +1,11 @@
 package kh.project.stayfit.common;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
@@ -21,8 +24,8 @@ public class Crawling {
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
 			options.addArguments("lang=ko_KR");
-			options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-			options.setExperimentalOption("useAutomationExtension", false);
+//			options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+//			options.setExperimentalOption("useAutomationExtension", false);
 
 			// Chrome 드라이버 인스턴스 설정
 			driver = new ChromeDriver(options);
@@ -33,80 +36,130 @@ public class Crawling {
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 
 			// URL로 접속
-			driver.get("https://login.coupang.com/login/login.pang?rtnUrl=https%3A%2F%2Fwww.coupang.com%2Fnp%2Fpost%2Flogin%3Fr%3Dhttps%253A%252F%252Fwww.coupang.com%252Fnp%252Fcategories%252F432482%253FlistSize%253D60%2526brand%253D%2526offerCondition%253D%2526filterType%253D%2526isPriceRange%253Dfalse%2526minPrice%253D%2526maxPrice%253D%2526page%253D1%2526channel%253Duser%2526fromComponent%253DY%2526selectedPlpKeepFilter%253D%2526sorter%253DbestAsc%2526filter%253D%2526component%253D432382%2526rating%253D0");
+			driver.get("https://www.fatsecret.kr/%EC%B9%BC%EB%A1%9C%EB%A6%AC-%EC%98%81%EC%96%91%EC%86%8C/");
 			
 			Thread.sleep(1000);
-			WebElement element = driver.findElement(By.xpath("//*[@id=\'login-email-input\']"));
-			element.sendKeys("qudwn980226@naver.com");
+			WebElement element = driver.findElement(By.xpath("//*[@id=\"ctl12_ctl04_ByFood\"]"));
+			element.click();
+			element.sendKeys("다신샵");
 			Thread.sleep(1000);
-			element = driver.findElement(By.xpath("//*[@id=\'login-password-input\']"));
-			element.sendKeys("chzh10424!");
+			element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[2]/div/div[1]/table/tbody/tr[2]/td/div/img"));
+			element.click();
 			Thread.sleep(1000);
-			element = driver.findElement(By.cssSelector("body > div.member-wrapper.member-wrapper--flex > div > div > form > div.login__content.login__content--trigger > button"));
-			element.sendKeys(Keys.ENTER);
 			
-			//WebElement element = driver.findElement(By.xpath("//*[@id='category432381']/li[1]/label"));
-			// 클릭한다. 사실 element.click()로도 클릭이 가능한데 가끔 호환성 에러가 발생하는 경우가 있다.
-			//js.executeScript("arguments[0].click();", element);
-//			WebElement element = driver.findElement(By.xpath("//*[@id=\'login\']/a"));
-//			element.sendKeys(Keys.ENTER);
+			Map<String, Object> allParams = new HashMap<>();
+			
+			ArrayList<String> locbrandList = new ArrayList<String>();
+			ArrayList<String> productList = new ArrayList<String>();
+			ArrayList<Double> sersizeList = new ArrayList<Double>();
+			ArrayList<String> capunitList = new ArrayList<String>();
+			ArrayList<Double> kcalList = new ArrayList<Double>();
+			ArrayList<Double> proteingList = new ArrayList<Double>();
+			ArrayList<Double> fatgList = new ArrayList<Double>();
+			ArrayList<Double> carbogList = new ArrayList<Double>();
+			
+			Boolean loop = true;
+			int z = 0;
+			while(loop) {
+				List<WebElement> elements = driver.findElements(By.cssSelector("a.prominent"));
+				int n = elements.size();
+				
+				for (int i = 0; i < n; i++) {
+					Thread.sleep(1000);
+					z = i;
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr[" + (z + 1) + "]/td/a[1]"));
+					element.click();
+					Thread.sleep(1000);
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/div/div/table/tbody/tr/td/div[2]/h2/a"));
+					locbrandList.add(element.getText());
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/div/div/table/tbody/tr/td/div[2]/h1"));
+					productList.add(element.getText());
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr/td[1]/div[1]/div[4]"));
+					if(element.getText().contains("(") && element.getText().contains(")")) { //1인분 (140 g) 형식으로 제공되는 경우
+						int idx1 = element.getText().indexOf("(");
+						int idx2 = element.getText().indexOf(")");
+						if(element.getText().substring(idx2-1, idx2).equals("g")) {
+							sersizeList.add(Double.parseDouble(element.getText().substring(idx1 + 1, idx2 - 2)));
+							capunitList.add(element.getText().substring(idx2-1, idx2));
+						} else {
+							sersizeList.add(Double.parseDouble(element.getText().substring(idx1 + 1, idx2 - 3)));
+							capunitList.add(element.getText().substring(idx2-2, idx2));
+						}
+					} else {
+						
+						
+						
+						if(element.getText().contains("g")) {
+							int idx1 = element.getText().indexOf("g");
+							sersizeList.add(Double.parseDouble(element.getText().substring(0, idx1 - 1)));
+							capunitList.add(element.getText().substring(idx1 - 1, idx1));
+						} else if(element.getText().contains("ml")) {
+							int idx1 = element.getText().indexOf("ml");
+							sersizeList.add(Double.parseDouble(element.getText().substring(0, idx1 - 2)));
+							capunitList.add(element.getText().substring(idx1 - 2, idx1));
+						}
+						
+						
+					}
+					
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr/td[1]/div[1]/div[12]"));
+					int idx = element.getText().indexOf(" kcal");
+					kcalList.add(Double.parseDouble(element.getText().substring(0, idx)));
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr/td[1]/div[1]/div[15]"));
+					idx = element.getText().indexOf("g");
+					proteingList.add(Double.parseDouble(element.getText().substring(0, idx)));
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr/td[1]/div[1]/div[21]"));
+					idx = element.getText().indexOf("g");
+					fatgList.add(Double.parseDouble(element.getText().substring(0, idx)));
+					
+					element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/table/tbody/tr/td[1]/div[1]/div[24]"));
+					idx = element.getText().indexOf("g");
+					carbogList.add(Double.parseDouble(element.getText().substring(0, idx)));
+					Thread.sleep(1000);
+					driver.navigate().back();
+				}
+				
+				element = driver.findElement(By.xpath("//*[@id=\"content\"]/table/tbody/tr/td[1]/div/div[3]/span[2]/a"));
+				
+				if(driver.findElements(By.cssSelector("#content > table > tbody > tr > td.leftCell > div > div.searchResultsPaging > span.next")).size() < 1) {
+					loop = false;
+				} else {
+					element.click();
+				}
+				
+			}
+			
+			allParams.put("locbrand", locbrandList);
+			allParams.put("product", productList);
+			allParams.put("sersize", sersizeList);
+			allParams.put("capunit", capunitList);
+			allParams.put("kcal", kcalList);
+			allParams.put("proteing", proteingList);
+			allParams.put("fatg", fatgList);
+			allParams.put("carbog", carbogList);
+			
+			for(int j = 0; j<locbrandList.size(); j++) {
+				System.out.println(locbrandList.get(j));
+				System.out.println(productList.get(j));
+				System.out.println(sersizeList.get(j));
+				System.out.println(capunitList.get(j));
+				System.out.println(kcalList.get(j));
+				System.out.println(proteingList.get(j));
+				System.out.println(fatgList.get(j));
+				System.out.println(carbogList.get(j));
+			}
 			
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-
-//			while (true) {
-//				try {
-//					// css selector로 element를 찾는다.
-//					element = driver.findElement(By.cssSelector("[href^='/626']"));
-//					// 클릭
-//					js.executeScript("arguments[0].click();", element);
-//					// 루프 정지
-//					break;
-//				} catch (Throwable e) {
-//					// 해당 element가 없으면 아래의 다음 페이지 element를 찾는다.
-//					System.out.println(driver.findElement(By.cssSelector(".page-warpper a.selected+a")));
-////					if() {
-//						element = driver.findElement(By.cssSelector(".page-warpper a.selected+a"));
-////					} else {
-////						element = driver.findElement(By.cssSelector(".page-warpper a.selected+a"));
-////					}
-// 					
-//					// 클릭
-//					js.executeScript("arguments[0].click();", element);
-//				}
-//			}
-//			// id가 promptEx인 데이터를 찾는다.
-//			element = driver.findElement(By.xpath("//*[@id='promptEx']"));
-//			// 버튼은 클릭이 되는데 link 계열은 script로 클릭해야 한다.
-//			element.click();
-//			// xpath로 팝업의 dom를 찾는다.
-//			element = driver.findElement(By.xpath("/html/body/div[6]/div/div/div[2]/div/form/input"));
-//			// input text에 test의 값을 넣는다.
-//			element.sendKeys("test");
-//			// 5초 기다린다.
-//			Thread.sleep(5000);
-//			// xpath로 팝업의 dom를 찾는다.
-//			element = driver.findElement(By.xpath("/html/body/div[6]/div/div/div[2]/div/form/input"));
-//			// 속성 value를 출력한다.
-//			System.out.println(element.getAttribute("value"));
-//			// .article의 글에 p 태그의 속성을 전부 가져온다.
-//			List<WebElement> elements = driver.findElements(By.cssSelector(".article p"));
-//			for (WebElement ele : elements) {
-//				// 속성의 NodeText를 전부 출력한다.
-//				System.out.println(ele.getText());
-//			}
 		} catch (Throwable e) {
 			e.printStackTrace();
 		} finally {
-//			driver.close();
+			//driver.close();
+			//driver.quit();
 		}
 	}
 	
