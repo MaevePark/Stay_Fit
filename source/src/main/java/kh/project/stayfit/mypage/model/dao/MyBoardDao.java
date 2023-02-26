6feadb76a145c996c22bbcba8bd5b1555cdfdd4a
ChocoJ1721031 <qudwn980226@naver.com>
2023-02-26 15:20:33 +0900

    게시판 페이징 추가

--- source/src/main/java/kh/project/stayfit/mypage/model/dao/MyBoardDao.java
+++ source/src/main/java/kh/project/stayfit/mypage/model/dao/MyBoardDao.java
@@ -67,7 +67,54 @@ public class MyBoardDao {
 		
 		return result;
 	}
-//	public int selectTotalCnt(int a, int b, String c) throws Exception {
-//		return sqlsession.selectOne("", );
-//	}
+	public int selectTotalCnt(
+			int mid
+			, String type
+			, int boardCategory
+			, int searchRange
+			, String searchword
+			) throws Exception {
+		
+		int result = 0;
+		
+		if(type.equals("bookmark")) {
+			Map<String, Object> paramList = new HashMap<String, Object>();
+			paramList.put("mid", mid);
+			paramList.put("boardCategory", boardCategory);
+			paramList.put("searchRange", searchRange);
+			paramList.put("searchword", searchword);
+			
+			result = sqlsession.selectOne("mypage.countBMark", paramList);
+			
+		} else if(type.equals("likereply")) {
+			Map<String, Object> paramList = new HashMap<String, Object>();
+			paramList.put("mid", mid);
+			paramList.put("boardCategory", boardCategory);
+			paramList.put("searchRange", searchRange);
+			paramList.put("searchword", searchword);
+			
+			result = sqlsession.selectOne("mypage.countLikeReply", paramList);
+			
+		} else if(type.equals("myboard")) {
+			Map<String, Object> paramList = new HashMap<String, Object>();
+			paramList.put("mid", mid);
+			paramList.put("boardCategory", boardCategory);
+			paramList.put("searchRange", searchRange);
+			paramList.put("searchword", searchword);
+			
+			result = sqlsession.selectOne("mypage.countMyBoard", paramList);
+			
+		} else if(type.equals("myreply")) {
+			Map<String, Object> paramList = new HashMap<String, Object>();
+			paramList.put("mid", mid);
+			paramList.put("boardCategory", boardCategory);
+			paramList.put("searchRange", searchRange);
+			paramList.put("searchword", searchword);
+			
+			result = sqlsession.selectOne("mypage.countMyReply", paramList);
+			
+		}
+		
+		return result;
+	}
 }
