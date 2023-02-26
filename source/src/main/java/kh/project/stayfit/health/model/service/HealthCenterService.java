f49d192339a838f3eb527bde0202a70a65f6e053
MinJiPark <pminji1@gmail.com>
2023-02-26 21:26:01 +0900

    healthCenter vo, service, controller 에 불필요한 요소 및 주석 삭제

--- source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterService.java
+++ source/src/main/java/kh/project/stayfit/health/model/service/HealthCenterService.java
@@ -5,5 +5,7 @@ import java.util.List;
 import kh.project.stayfit.health.model.vo.HealthCenter;
 
 public interface HealthCenterService {
+	
+	//보건소 list 목록 출력
 	public List<HealthCenter> selectList() throws Exception;
 }
