6011d009571559915c8b91a80bc890ed99d7f6a7
MinJiPark <pminji1@gmail.com>
2023-02-28 16:49:30 +0900

    HealthInfoForCalorie vo 주석 추가

--- source/src/main/java/kh/project/stayfit/health/model/vo/HealthInfoForCalorie.java
+++ source/src/main/java/kh/project/stayfit/health/model/vo/HealthInfoForCalorie.java
@@ -11,12 +11,12 @@ import lombok.ToString;
 @Setter
 @Component
 public class HealthInfoForCalorie {
-	private String bmr;
-	private String activityMetabolicRate;
-	private String digestionEnergy;
-	private String age;
-	private String sex;
-	private String stature;
-	private String weight;
-	private String goal_weight;
+	private String bmr; // 기초대사량
+	private String activityMetabolicRate; // 활동대사량
+	private String digestionEnergy; // 소화를 위한 에너지
+	private String age; // 나이
+	private String sex; // 성별
+	private String stature; // 키
+	private String weight; // 몸무게
+	private String goal_weight; // 목표 체중
 }
