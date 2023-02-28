cc812a814191556d96bc85a40f40f89654d45da4
ChocoJ1721031 <qudwn980226@naver.com>
2023-03-01 01:32:46 +0900

    임포트 누락 수정

--- source/src/main/java/kh/project/stayfit/board/controller/BoardController.java
+++ source/src/main/java/kh/project/stayfit/board/controller/BoardController.java
@@ -1,8 +1,7 @@
 package kh.project.stayfit.board.controller;
 
 import java.util.List;
-
-
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import kh.project.stayfit.board.model.service.BoardService;
 import kh.project.stayfit.board.model.vo.Board;
+import kh.project.stayfit.common.Paging;
 
 
 @Controller
