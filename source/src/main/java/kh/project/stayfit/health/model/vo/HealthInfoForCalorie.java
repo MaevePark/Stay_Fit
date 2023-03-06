907ce0f387704cb3caf78506060076838702f6ce
MinJiPark <pminji1@gmail.com>
2023-03-06 22:10:34 +0900

    LocationCode VO 'goal_term_type' 체중 감량 기간 타입 추가 및 순서 변경

--- source/src/main/java/kh/project/stayfit/health/model/vo/HealthInfoForCalorie.java
+++ source/src/main/java/kh/project/stayfit/health/model/vo/HealthInfoForCalorie.java
@@ -11,12 +11,14 @@ import lombok.ToString;
 @Setter
 @Component
 public class HealthInfoForCalorie {
-	private String bmr; // 기초대사량
-	private String activityMetabolicRate; // 활동대사량
-	private String digestionEnergy; // 소화를 위한 에너지
-	private String age; // 연령
-	private String gender; // 성별
+	private String gender; // 성별 (여,남)
 	private String stature; // 키
-	private String weight; // 몸무게
+	private String weight; // 체중
 	private String goal_weight; // 목표 체중
+	private String age; // 연령
+	private String goal_term; // 체중 감량 기간
+	private String goal_term_type; // 체중 감량 기간 타입 (개월,일)
+	private String bmr; // 기초대사량
+	private String amr; // 활동대사량
+	private String digestionEnergy; // 소화를 위한 에너지
 }
