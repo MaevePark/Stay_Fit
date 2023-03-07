5e7d753883e60e223a2fb5e7352083864a216201
ChocoJ1721031 <qudwn980226@naver.com>
2023-03-07 09:59:12 +0900

    찜목록 javascript 주석 삭제 및 불필요 코드 삭제

--- source/src/main/webapp/resources/js/mypage/wish.js
+++ source/src/main/webapp/resources/js/mypage/wish.js
@@ -1,12 +1,12 @@
 window.onload = function() {
 	
-	$('.delWish').click(function() {
-		delWish(this);
-	});
-	
-	$('.fa-shopping-cart').click(function() {
-		sendCart(this);
-	});
+//	$('.delWish').click(function() {
+//		delWish(this);
+//	});
+//	
+//	$('.fa-shopping-cart').click(function() {
+//		sendCart(this);
+//	});
 	
 	
 }
@@ -28,8 +28,7 @@ function delWish(el) {
 		, dataType: "json"
 		, success: function(result) {
 			console.log(result);
-			location.href="wish";
-			//loadWish(mid);
+			loadWish(mid);
 		}
 		, error: function(request, status, errordata) {
 			alert("error code: "+request.status +"\n"
@@ -54,8 +53,7 @@ function sendCart(el) {
 		, dataType: "json"
 		, success: function(result) {
 			console.log(result);
-			location.href="wish";
-			
+			loadWish(mid);
 		}
 		, error: function(request, status, errordata) {
 			alert("error code: "+request.status +"\n"
@@ -67,56 +65,59 @@ function sendCart(el) {
 
 
 function loadWish(el) {
-//	$.ajax({
-//		url:"loadwish"
-//		, method:"get"
-//		, dataType: "json"
-//		, data: {
-//			mid : el
-//		}
-//		, dataType: "json"
-//		, success: function(result) {
-//			console.log(result);
-//			console.log(document.querySelector(".container .row div .row"));
-//			const parent = document.querySelector(".container .row div .row");
-//			parent.innerHTML = '<c:if test="${wishList.size() > 0 }">'+
-//									'<c:forEach items="${wishList }" var="list">'+
-//									'<div class="col-lg-4 col-md-6 col-sm-6">'+
-//										'<div class="product__item">'+
-//											'<div class="product__item__pic set-bg"'+
-//												'data-setbg="${list.pimage }">'+
-//												'<ul class="product__item__pic__hover">'+
-//													'<li><a class="delWish" data-pid="${list.pid }">X</a></li>'+
-//													'<li><a class="fa fa-shopping-cart" data-pid="${list.pid }"></a></li>'+
-//												'</ul>'+
-//											'</div>'+
-//											'<div class="product__item__text">'+
-//												'<h6>'+
-//													'<a href="#">${list.pname }</a>'+
-//												'</h6>'+
-//												'<h5>${list.pprice }원</h5>'+
-//											'</div>'+
-//										'</div>'+
-//									'</div>'+
-//								'</c:forEach>'+
-//								'<!-- 페에징 -->'+
-//								'<jsp:include page="/WEB-INF/views/paging.jsp" />'+
-//							'</c:if>'+
-//							'<c:if test="${wishList.size() <= 0 }">'+
-//								'<h3>찜목록에 등록된 상품이 없습니다.</h3>'+
-//							'</c:if>';
-//		}
-//		, error: function(request, status, errordata) {
-//			alert("error code: "+request.status +"\n"
-//					+"message: "+request.responseText +"\n"
-//					+"error: "+errordata+"\n");
-//		}
-//	});
+	$.ajax({
+		url:"loadwish"
+		, method:"get"
+		, dataType: "json"
+		, data: {
+			mid : el
+		}
+		, dataType: "json"
+		, success: function(result) {
+			console.log(result);
+			var wishList = result.wishList;
+			const parent = document.querySelector(".container .row div .row");
+			var html = "";
+			
+			if(wishList.length > 0) {
+				for(let i=0; i<wishList.length; i++) {
+					var list = wishList[i];
+					html += '<div class="col-lg-4 col-md-6 col-sm-6">'+
+									'<div class="product__item">'+
+									'<div class="product__item__pic set-bg" '+
+										'data-setbg="'+list.pimage+'">'+
+										'<ul class="product__item__pic__hover">'+
+											'<li><a class="delWish" data-pid="'+list.pid+'" onclick="delWish(this)">X</a></li>'+
+											'<li><a class="fa fa-shopping-cart" data-pid="'+list.pid+'" onclick="sendCart(this)"></a></li>'+
+										'</ul>'+
+									'</div>'+
+									'<div class="product__item__text">'+
+										'<h6>'+
+											'<a href="#">'+list.pname+'</a>'+
+										'</h6>'+
+										'<h5>'+list.pprice+'원</h5>'+
+									'</div>'+
+								'</div>'+
+							'</div>';
+				}
+				html += '<jsp:include page="/WEB-INF/views/paging.jsp" />';
+			} else {
+				html +='<h3>찜목록에 등록된 상품이 없습니다.</h3>';
+			}
+			parent.innerHTML = html;
+			$('.set-bg').each(function () {
+		        var bg = $(this).data('setbg');
+		        $(this).css('background-image', 'url(' + bg + ')');
+		    });
+		}
+		, error: function(request, status, errordata) {
+			alert("error code: "+request.status +"\n"
+					+"message: "+request.responseText +"\n"
+					+"error: "+errordata+"\n");
+		}
+	});
 	
 }
-
-
-
 
 
 
