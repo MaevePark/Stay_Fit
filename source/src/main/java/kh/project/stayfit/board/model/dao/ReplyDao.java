c054720430468b7a256658ca0a5a1836c10caf55
HJ <down012459@gmail.com>
2023-03-11 21:42:00 +0900

    board bookmark

--- source/src/main/java/kh/project/stayfit/board/model/dao/ReplyDao.java
+++ source/src/main/java/kh/project/stayfit/board/model/dao/ReplyDao.java
@@ -15,67 +15,89 @@ import kh.project.stayfit.board.model.vo.Reply;
 public class ReplyDao {
 	@Autowired
 	private SqlSession sqlsession;
-	
-	//댓글list
+
+	// 댓글list
 	public List<Reply> replylist(int bid) {
 		List<Reply> rlist = sqlsession.selectList("board.boardreply", bid);
 		return rlist;
 	}
-	//부모 댓글 작성
-	public int write(Reply vo) throws Exception{
+
+	// 부모 댓글 작성
+	public int write(Reply vo) throws Exception {
 		return sqlsession.insert("board.rwrite", vo);
 	}
-	
-	//댓글 수정
-	public int update(Reply vo) throws Exception{
+
+	// 댓글 수정
+	public int update(Reply vo) throws Exception {
 		return sqlsession.insert("board.rupdate", vo);
 	}
-	
-	//댓글 삭제
-	public int delete(int rid) throws Exception{
+
+	// 댓글 삭제
+	public int delete(int rid) throws Exception {
 		return sqlsession.delete("board.rdelete", rid);
 	}
-	
-	//댓글 좋아요 카운트
-	public int checklike(int rid, int mid) throws Exception{
+
+	// 댓글 좋아요 카운트
+	public int checklike(int rid, int mid) throws Exception {
 		Map<String, Object> map = new HashMap<>();
 		map.put("rid", rid);
 		map.put("mid", mid);
 		return sqlsession.selectOne("board.checklike", map);
 	}
-	
-	//댓글 좋아요
-	public int like(int rid, int mid) throws Exception{
+
+	// 댓글 좋아요
+	public int like(int rid, int mid) throws Exception {
 		Map<String, Object> map = new HashMap<>();
-	    map.put("rid", rid);
-	    map.put("mid", mid);
+		map.put("rid", rid);
+		map.put("mid", mid);
 		return sqlsession.update("board.rlike", map);
 	}
-	
-	//댓글 좋아요 삭제
+
+	// 댓글 좋아요 삭제
 	public int unlike(int rid, int mid) throws Exception {
-	    Map<String, Object> map = new HashMap<>();
-	    map.put("rid", rid);
-	    map.put("mid", mid);
-	    return sqlsession.delete("board.dellike", map);
+		Map<String, Object> map = new HashMap<>();
+		map.put("rid", rid);
+		map.put("mid", mid);
+		return sqlsession.delete("board.dellike", map);
 	}
-	
+
+	// 북마크 카운트
+	public int checkbook(int mid, int bid) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("mid", mid);
+		map.put("bid", bid);
+		return sqlsession.selectOne("board.checkbook", map);
+	}
+
+	// 북마크
+	public int book(int mid, int bid) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("mid", mid);
+		map.put("bid", bid);
+		return sqlsession.update("board.book", map);
+	}
+
+	// 북마크 취소
+	public int delbook(int mid, int bid) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("mid", mid);
+		map.put("bid", bid);
+		return sqlsession.delete("board.delbook", map);
+	}
+
 //	//대댓글 작성
 //	public int answer(Reply) throws Exception{
 //		return sqlsession.;
 //	}
-
-
 
 //	//댓글 신고
 //	public int report(Reply, int) throws Exception{
 //		return sqlsession.;
 //	}
 
-	
 //	//답변 선택
 //	public int adopt(int, int, int, int) throws Exception{
 //		return sqlsession.;
 //	}
-	
+
 }
--- source/src/main/java/kh/project/stayfit/board/model/service/ReplyService.java
+++ source/src/main/java/kh/project/stayfit/board/model/service/ReplyService.java
@@ -5,37 +5,42 @@ import java.util.List;
 import kh.project.stayfit.board.model.vo.Reply;
 
 public interface ReplyService {
-	
-	//댓글 조회
+
+	// 댓글 조회
 	public List<Reply> replylist(int bid) throws Exception;
-	
-	//부모 댓글 작성
+
+	// 부모 댓글 작성
 	public int write(Reply vo) throws Exception;
-	
-	//댓글 수정
+
+	// 댓글 수정
 	public int update(Reply vo) throws Exception;
-	
-	//댓글 삭제
+
+	// 댓글 삭제
 	public int delete(int rid) throws Exception;
 
-	//댓글 좋아요 카운트용
+	// 댓글 좋아요 카운트용
 	public int checklike(int rid, int mid) throws Exception;
-	
-	//댓글 좋아요
+
+	// 댓글 좋아요
 	public int like(int rid, int mid) throws Exception;
-	
-	//댓글 좋아요 삭제
+
+	// 댓글 좋아요 삭제
 	public int unlike(int rid, int mid) throws Exception;
 
-	//	//대댓글 작성
+	// 북마크 카운트
+	public int checkbook(int mid, int bid) throws Exception;
+
+	// 북마크
+	public int book(int mid, int bid) throws Exception;
+
+	// 북마크 취소
+	public int delbook(int mid, int bid) throws Exception;
+
+	// //대댓글 작성
 //	public int answer(Reply) throws Exception;
-	
-
-
 
 //	//댓글 신고
 //	public int report(Reply, int) throws Exception;
-
 
 //	//답변 선택
 //	public int adopt(int, int, int, int) throws Exception;
