a7cb706fc97ab029c1a26eb950556c73d1759d18
HJ <down012459@gmail.com>
2023-03-15 21:45:03 +0900

    main exception

--- source/src/main/java/kh/project/stayfit/main/controller/MainController.java
+++ source/src/main/java/kh/project/stayfit/main/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,21 +21,32 @@ public class MainController {
 
 	@GetMapping({ "/", "/main", "/index" })
 	public ModelAndView mainPage(ModelAndView mv) throws Exception {
-		//공지사항
+		// 공지사항
 		List<Board> nolist = srv.notice();
 		mv.addObject("notice", nolist);
-		//인기글
+		// 인기글
 		List<Board> popular = srv.popular();
 		mv.addObject("popular", popular);
-		//최신글
+		// 최신글
 		List<Board> recent = srv.recent();
 		mv.addObject("recent", recent);
-		
-		//세일품목
+
+		// 세일품목
 		mv.addObject("saleList", shopService.selectSaleList());
-		
+
 		mv.addObject("sectionName", "main.jsp");
 		mv.setViewName("index");
+		return mv;
+	}
+
+	// 오류
+	@ExceptionHandler(Exception.class) // 모든 Exception시 여기로
+	public ModelAndView exceptionHandler(Exception e /* ModelAndView mv 작성시 오류 발생 */) {
+		// 프로젝트초기에는 e.printStackTrace(); 로 로그 봐야함. 나중에 지우기
+		e.printStackTrace();
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("errMsg", e.getMessage());
+		mv.setViewName("/AccessDenied");
 		return mv;
 	}
 }
